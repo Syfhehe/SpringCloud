a34cd92a683094bbadab2b2478c628ddf06c26c1
syfshen <syfshen@cn.ibm.com>
2017-04-06 16:50:15 +0800

    增加server

--- eureka-server/src/main/java/com/didispace/Application.java
+++ eureka-server/src/main/java/com/didispace/Application.java
@@ -2,9 +2,9 @@ package com.didispace;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
